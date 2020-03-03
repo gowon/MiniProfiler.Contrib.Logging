1b33ed126624e396e483b34b2548e348ac4b3c80
Gowon Patterson <gowon.ocp@gmail.com>
2020-03-03 02:07:31 -0500

    Initial code commit

---
+++ src/MiniProfiler.Contrib.Logging/LoggingProvider.cs
@@ -0,0 +1,110 @@
+﻿namespace StackExchange.Contrib.Profiling.Storage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using StackExchange.Profiling;
+    using StackExchange.Profiling.Storage;
+
+    public class LoggingProvider : IAsyncStorage
+    {
+        private readonly Func<MiniProfiler, object> _formatter;
+        private readonly ILogger _logger;
+        private readonly LogLevel _profilingLevel;
+
+        public LoggingProvider(ILogger logger, LogLevel profilingLevel = LogLevel.Trace,
+            Func<MiniProfiler, object> formatter = null)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _profilingLevel = profilingLevel;
+            _formatter = formatter ?? (profiler =>
+            {
+                var json = JsonConvert.SerializeObject(profiler);
+                var obj = JsonConvert.DeserializeObject<IDictionary<string, object>>(json,
+                    new RecursiveDictionaryConverter());
+
+                return new Dictionary<string, object> {[nameof(MiniProfiler)] = obj};
+            });
+        }
+
+        public void Save(MiniProfiler profiler)
+        {
+            if (!_logger.IsEnabled(_profilingLevel))
+            {
+                return;
+            }
+
+            using (_logger.BeginScope(_formatter.Invoke(MiniProfiler.Current)))
+            {
+                _logger.Log(_profilingLevel, MiniProfiler.Current.RenderPlainText());
+            }
+        }
+
+        public MiniProfiler Load(Guid id)
+        {
+            return null;
+        }
+
+        public async Task SaveAsync(MiniProfiler profiler)
+        {
+            Save(profiler);
+            await Task.CompletedTask;
+        }
+
+        public IEnumerable<Guid> List(
+            int maxResults,
+            DateTime? start = null,
+            DateTime? finish = null,
+            ListResultsOrder orderBy = ListResultsOrder.Descending)
+        {
+            return Enumerable.Empty<Guid>();
+        }
+
+        public Task<IEnumerable<Guid>> ListAsync(
+            int maxResults,
+            DateTime? start = null,
+            DateTime? finish = null,
+            ListResultsOrder orderBy = ListResultsOrder.Descending)
+        {
+            return Task.FromResult(Enumerable.Empty<Guid>());
+        }
+
+        public Task<MiniProfiler> LoadAsync(Guid id)
+        {
+            return Task.FromResult((MiniProfiler) null);
+        }
+
+        public void SetUnviewed(string user, Guid id)
+        {
+            /* no-op */
+        }
+
+        public Task SetUnviewedAsync(string user, Guid id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void SetViewed(string user, Guid id)
+        {
+            /* no-op */
+        }
+
+        public Task SetViewedAsync(string user, Guid id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public List<Guid> GetUnviewedIds(string user)
+        {
+            return new List<Guid>();
+        }
+
+        public Task<List<Guid>> GetUnviewedIdsAsync(string user)
+        {
+            return Task.FromResult(new List<Guid>());
+        }
+    }
+}
---
+++ src/MiniProfiler.Contrib.Logging/RecursiveDictionaryConverter.cs
@@ -0,0 +1,59 @@
+﻿namespace StackExchange.Contrib.Profiling.Storage
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    // https://github.com/okta/okta-sdk-dotnet/blob/master/src/Okta.Sdk/Internal/RecursiveDictionaryConverter.cs
+    // this would also work, but seemed "hacky" https://stackoverflow.com/a/19140420/7644876
+    public sealed class RecursiveDictionaryConverter : CustomCreationConverter<IDictionary<string, object>>
+    {
+        /// <inheritdoc />
+        public override IDictionary<string, object> Create(Type objectType)
+        {
+            return new Dictionary<string, object>(StringComparer.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public override bool CanConvert(Type objectType)
+            // We want to handle explicit objects and
+            // also nested objects (which might be dictionaries)
+        {
+            return objectType == typeof(object);
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            // Deserialize nested objects as dictionaries
+            var isObject = reader.TokenType == JsonToken.StartObject || reader.TokenType == JsonToken.Null;
+            if (isObject)
+            {
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+            }
+
+            // Deserialize arrays as List<object>
+            var isArray = reader.TokenType == JsonToken.StartArray;
+            if (isArray)
+            {
+                var list = new List<object>();
+
+                while (reader.Read() && reader.TokenType != JsonToken.EndArray)
+                {
+                    var listObject = reader.TokenType == JsonToken.StartObject
+                        ? base.ReadJson(reader, objectType, existingValue, serializer)
+                        : serializer.Deserialize(reader);
+
+                    list.Add(listObject);
+                }
+
+                return list;
+            }
+
+            // If not, fall back to standard deserialization (for numbers, etc)
+            return serializer.Deserialize(reader);
+        }
+    }
+}
